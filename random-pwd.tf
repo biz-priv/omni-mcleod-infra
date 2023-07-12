6607df86cb7d179810d9c45a65ebe36af4fcf5ee
sreddybiz <sreddy@bizcloudexperts.com>
2023-07-13 01:15:13 +0530

    changes in password param

--- random-pwd.tf
+++ random-pwd.tf
@@ -1,9 +1,3 @@
-resource "random_string" "username" {
-  length           = 16
-  special          = true
-  override_special = "mcl/@£$"
-}
-
 resource "random_password" "password" {
   length           = 16
   special          = true
