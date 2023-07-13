291a1ca25cbee7130699d348fdf2d99ee3fca44e
sreddybiz <sreddy@bizcloudexperts.com>
2023-07-13 16:16:34 +0530

    changes in params

--- ssm.tf
+++ ssm.tf
@@ -1,16 +1,3 @@
-resource "aws_ssm_parameter" "mcl_rds_master_password" {
-  name  = "/omni-mcleod/${var.env}/rds/db.password"
-  type  = "SecureString"
-  value = random_password.password.result
-
-  tags = {
-    Application = "Mcleod NS Integration"
-    CreatedBy   = "BizCloudExperts"
-    Environment = var.env
-    STAGE       = var.env
-  }
-}
-
 resource "aws_ssm_parameter" "omni_dw_rds_master_password" {
   name  = "/omni-dw/${var.env}/rds/db.password"
   type  = "SecureString"
